8f2340e6b7041078e6b85cd664d8c6a80d992982
Sid2202 <sidpatil2209@gmail.com>
2024-02-05 12:30:42 +0530

    simple task done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -28,95 +28,51 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      resizeToAvoidBottomInset: true,
       body: ListView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         children: [
-          Container(
-            padding: EdgeInsets.all(32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Hi Sid,',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  '12 January, 2024',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            decoration: const BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.only(left: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Today',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                // const SizedBox(height: 10),
-              ],
-            ),
-          ),
+          header(),
+          subheader(),
           for (Task task in tasks)
             Column(
               children: [
-                ListTile(
-                  onTap: () {
-                    // Add your onPressed logic here!
-                  },
-                  leading: Transform.scale(
-                    scale: 0.9,
-                    child: Checkbox(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                if(!task.done) ...{
+                  ListTile(
+                    onTap: () {
+                      // Add your onPressed logic here!
+                    },
+                    leading: Transform.scale(
+                      scale: 0.9,
+                      child: Checkbox(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        value: task.done,
+                        onChanged: (value) {
+                          setState(() {
+                            task.done = value!;
+                          });
+                        },
                       ),
-                      value: task.done,
-                      onChanged: (value) {
-                        setState(() {
-                          task.done = value!;
-                        });
-                      },
+                    ),
+                    title: Text(
+                      task.title,
+                      style: TextStyle(
+                        decoration: task.done
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
-                  title: Text(
-                    task.title,
-                    style: TextStyle(
-                      decoration: task.done
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none,
-                      fontSize: 16,
-                      color: Colors.black,
-                    ),
+                  const Divider(
+                    height: 1,
+                    thickness: 1,
+                    indent: 20,
                   ),
-                ),
-                const Divider(
-                  height: 1,
-                  thickness: 1,
-                  indent: 20,
-                ),
+                },
               ],
             ),
           // ListTile(
@@ -137,19 +93,19 @@ class _HomePageState extends State<HomePage> {
           //   thickness: 1,
           //   indent: 20,
           // ),
-          if (!isAddingTask) ...[
-            buildTaskTextField(),
-            const Divider(
-              height: 1,
-              thickness: 1,
-              indent: 20,
-            ),
-          ],
+          // if (!isAddingTask) ...[
+          //   buildTaskTextField(),
+          //   const Divider(
+          //     height: 1,
+          //     thickness: 1,
+          //     indent: 20,
+          //   ),
+          // ],
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          isAddingTask = !isAddingTask;
+          _showAddTaskBottomSheet(context);
         },
         child: const Icon(Icons.add),
         backgroundColor: Colors.black,
@@ -157,74 +113,108 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget buildTaskTextField() {
-    return TextField(
-      autofocus: true,
-      controller: _newTaskController,
-      onSubmitted: _addTask,
-      decoration: InputDecoration(
-        hintText: 'What do you want to do?',
-        hintStyle: TextStyle(
-          fontSize: 16,
-          color: Colors.grey[400],
-        ),
-        border: InputBorder.none,
-        contentPadding: EdgeInsets.only(bottom: 4),
-      ),
+  void _showAddTaskBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          // padding: EdgeInsets.all(16),
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  hintText: 'What do you want to do?',
+                ),
+                onSubmitted: (value) {
+                  // Handle the submitted value (new task)
+                  if (value != null && value.trim().isNotEmpty) {
+                    setState(() {
+                      tasks.add(Task(value, false));
+                    });
+                    Navigator.pop(context); // Close the bottom sheet
+                  }
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
-  }
-
-  void _addTask(String value) {
-    if (value != null && value.trim().isNotEmpty) {
-      setState(() {
-        tasks.add(Task(value, false));
-        isAddingTask = false;
-      });
-    }
-    _newTaskController.clear();
   }
 }
-  // void _startAddingTask(BuildContext context) {
-  //   FocusScope.of(context).requestFocus(new FocusNode());
-  //   showTextField(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return TextField(
-  //             autofocus: true,
-  //             controller: _newTaskController,
-  //             onSubmitted: (value) {
-  //               if(value != null && value.trim().isNotEmpty){
-  //                 setState(() {
-  //                   tasks.add(Task(value, false));
-  //                 });
-  //               }
-  //               _newTaskController.clear();
-  //             },
-  //             decoration: InputDecoration(
-  //               hintText: 'What do you want to do?',
-  //               hintStyle: TextStyle(
-  //                 fontSize: 16,
-  //                 color: Colors.grey[400],
-  //               ),
-  //               border: InputBorder.none,
-  //               contentPadding: EdgeInsets.only(bottom: 4),
-  //             ),
-  //           ),
-  //     },
-  //   );
-  // }
 
-  AppBar appBar() {
-    return AppBar(
-      title: const Text(
-        'GetShitDone',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-        ),
+AppBar appBar() {
+  return AppBar(
+    title: const Text(
+      'GetShitDone',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
       ),
-      backgroundColor: Colors.black,
-      elevation: 0,
-    );
-  }
+    ),
+    backgroundColor: Colors.black,
+    elevation: 0,
+  );
+}
+
+header() {
+  return Container(
+    padding: EdgeInsets.all(32),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Hi Sid,',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          '12 January, 2024',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+    decoration: const BoxDecoration(
+      color: Colors.black,
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(50),
+        bottomRight: Radius.circular(50),
+      ),
+    ),
+  );
+}
+
+subheader() {
+  return Container(
+    padding: EdgeInsets.all(12),
+    margin: EdgeInsets.only(left: 8),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Today',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+  );
+}
 
